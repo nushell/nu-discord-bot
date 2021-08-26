62880355754651012783507f42a4045d629608c5
Tanishq Kancharla <tklightningfast@gmail.com>
2021-08-25 20:44:17 -0400

    Made serenity bot

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #[macro_use] extern crate rocket;
+use nu_engine::EvaluationContext;
+use nu_errors::ShellError;
 use rocket::serde::json::Json;
 use rocket::serde::{Deserialize};
 use nu_cli::parse_and_eval;
@@ -6,37 +8,96 @@ mod context;
 mod run_external;
 use context::create_sandboxed_context;
 
-#[derive(Deserialize)]
-#[serde(crate = "rocket::serde")]
-struct Command<'r> {
-    input: &'r str
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+
+        command,
+        group
+    }
+};
+use dotenv::dotenv;
+use serenity::model::prelude::Ready;
+use std::env;
+use std::error::Error;
+
+#[command]
+async fn about(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "A simple test bot").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+    Ok(())
+}
+
+#[group]
+#[commands(about, ping)]
+struct General;
+
+struct Handler {
+    sandbox: EvaluationContext
 }
 
 
-#[post("/", format = "json", data = "<command>")]
-fn run_nu(command: Json<Command<'_>>) -> String {
-    if let Ok(context) = create_sandboxed_context() {
-        match parse_and_eval(command.input, &context) {
-            Ok(result) => {
-                result
-            },
-            Err(error) => {
-                error.to_string()
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if let Some(command) = msg.content.trim().strip_prefix("nu! ") {
+            println!("Received command: {}", command);
+            let result = match parse_and_eval(command, &self.sandbox) {
+                Ok(res) => {
+                    format!("`{}`", res.to_string())
+                },
+                Err(why) => {
+                    format!("`{}`", why.to_string())
+                }
+            };
+
+            if let Err(e) = msg.reply(ctx, result).await {
+                println!("Error when replying to message: {}", e);
             }
         }
-    } else {
-        "Failed to create a context!".to_string()
+        
     }
-    
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
 }
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn create_context() -> Result<EvaluationContext, ShellError> {
+    Ok(create_sandboxed_context()?)
 }
 
-#[launch]
-fn rocket() -> _ {
-    let rocket = rocket::build();
-    rocket.mount("/", routes![index, run_nu])
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
+
+    let sandbox = create_context().await?;
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("nu! ")) 
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let mut client = Client::builder(token)
+        .event_handler(Handler {sandbox})
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+    Ok(())
 }
